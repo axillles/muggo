2e18613c06d438a6bf4343169d5093b32e6b3b74
Axillles <gavrilovgavrilov228@gmail.com>
2025-04-21 16:53:11 +0300

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Артем Гаврилов on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeAppTests/CoffeeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppTests.swift
+//  CoffeeAppTests
+//
+//  Created by Артем Гаврилов on 21.04.25.
+//
+
+import Testing
+@testable import CoffeeApp
+
+struct CoffeeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
